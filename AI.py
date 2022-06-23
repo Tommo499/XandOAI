39f7094f612b2f4a403121520221aa90e180bbcb
Tommo499 <73698910+Tommo499@users.noreply.github.com>
2022-06-23 21:52:48 +0930

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,27 +1,52 @@
-# import random as r
-import json
 from math import sqrt
 
 c = sqrt(2)
+datafolderprefix = "C:\\Users\\Thomas\\Documents\\XandOdata*.json"
 
-def choose_child(round, parent):
-    with open(f"C:\\Users\\twach\\PycharmProjects\\XandO\\XandOdata{round}.json") as data:
-        board = json.load(data)
-        boarddata = dict()
-        for thing in board:
-            _ = list(eval(thing))
-            boarddata[_[0]] = eval(thing)[_[0]]
-        print(boarddata)
-    # with relevant data open, find children and their data
-    """
-    for child in children:
+
+def fetch_json(j):  # converts json string to dictionary
+    import json
+    temp1 = json.load(j)
+    return_val = dict()
+    for item in temp1:
+        _ = list(eval(item))
+        return_val[_[0]] = eval(item)[_[0]]
+    return return_val
+
+
+def fetchfromfile(filename):  # converts file with json string to dictionary
+    with open(filename) as file:
+        return_value = fetch_json(file)
+    return return_value
+
+
+def find_children(parentname):
+    children = []
+    for potential_child in range(1, 10):
+        if str(potential_child) not in parentname:
+            children.append(parentname + str(potential_child))
+    return children
+
+
+def getchilddata(parent):
+    child_file = fetchfromfile(datafolderprefix.replace('*', str(len(parent)+1)))
+    for child in child_file:
+        if not child.startswith(parent):
+            child_file.pop(child)
+    return child_file
+
+
+def choose_child(roundnum, parent):
+    boarddata = fetchfromfile(datafolderprefix.replace('*', str(roundnum)))
+    print(getchilddata('1'))
+
+    for child in find_children(parent):
         wins = 0
         plays = 0
         if plays == 0:
             win_ratio = 0
         else:
             win_ratio = wins / plays
-         """
 
     return 0
 
