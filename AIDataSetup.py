5b1df2da6e9b880f0f9216fd4bfaef8b8edbed15
Tommo499 <73698910+Tommo499@users.noreply.github.com>
2022-07-07 08:52:29 +0930

    Update AIDataSetup.py

--- AIDataSetup.py
+++ AIDataSetup.py
@@ -1,20 +1,46 @@
 from itertools import permutations
 import json
-import pprint
+import math
+
+winConditions = ('123', '456', '789', '147', '258', '369', '159', '357')
+# ↑ will be useful to make the redundancy checker
+
+
+def fully_contained(sub, container):
+    for item in sub:
+        if not item in container:
+            return False
+    return True
+
+
+def redundant(game):  # checks if the game could have been ended earlier, only for 
+    if len(game) >= 6:
+        for segment in range(5, len(game) + 1):
+            partgame = game[:segment]
+            player1 = ''.join(sorted(list(partgame[0::2])))
+            player2 = ''.join(sorted(list(partgame[1::2])))
+            for item in winConditions:
+                if fully_contained(item, player1) and segment < len(game):
+                    return True
+                elif fully_contained(item, player2) and segment < len(game):
+                    return True
+    return False
+
 
 spots = '123456789'
 jsonList = []
 
+""" When uncommenting, replace the file path with your file path
 for leng in range(1,10):
     jsonList = []
     for branch in permutations(spots, leng):
-        temp = {''.join(branch): {'w': 0, 'p': 0, 'parent': ''.join(branch[:-1])}}
-        jsonList.append(json.dumps(temp))
-        del temp
-    json.dump(jsonList, open(f"C:\\Users\\twach\\PycharmProjects\\XandO\\XandOdata{leng}.json", 'w'))
+        if not redundant(branch):
+            temp = {''.join(branch): {'w': 0, 'p': 0}}
+            jsonList.append(json.dumps(temp))
+            del temp
+    json.dump(jsonList, open(f"C:\\Users\\Thomas\\Documents\\XandOdata{leng}.json", 'w'))
     del jsonList
 
-#json.dump(jsonList, open("C:\\Users\\twach\\PycharmProjects\\XandO\\XandOboard.json", 'w'))
 
-#pprint.pprint(jsonList)
-# Todo # Make another json file to put the constant and the simulation counter
+temp = {'simulations': 1, "c": math.sqrt(2)}
+"""
