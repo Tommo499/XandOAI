745cf82545984aa2c0a4218a274940c5b474794b
Tommo499 <73698910+Tommo499@users.noreply.github.com>
2022-07-28 13:50:50 +0930

    Cleaned up some of the code

--- AI.py
+++ AI.py
@@ -1,27 +1,30 @@
 from math import sqrt, log, e
 import random as r
 import json
+import os
 
-
-datafolderprefix = "C:\\Users\\Thomas\\Documents\\XandOdata*.json"  # The asterisk gets replaced with the file number
+datafolderprefix = os.path.dirname(os.path.realpath(__file__)) + '\\XandOdata*.json'  # use this if the data is in the
+#                                                                                     same directory as this file
 first = None
 prettyboard = f" 7 ███ 8 ███ 9\n" \
-        f"███████████████\n" \
-        f" 4 ███ 5 ███ 6\n" \
-        f"███████████████\n" \
-        f" 1 ███ 2 ███ 3\n"
+              f"███████████████\n" \
+              f" 4 ███ 5 ███ 6\n" \
+              f"███████████████\n" \
+              f" 1 ███ 2 ███ 3\n"
 
 # constants for value algorithm
 c = sqrt(2)
 simulations = 1
 
-winConditions = ('123', '456', '789', '147', '258', '369', '159', '357')
+winconditions = ('123', '456', '789', '147', '258', '369', '159', '357')
 
-def fully_contained(sub: str, container: str):
+
+def fullycontained(sub: str, container: str):
     for item in sub:
         if item not in container:
             return False
     return True
+
 
 def fetchjson(j):  # converts json string to dictionary
     import json
@@ -32,82 +35,91 @@ def fetchjson(j):  # converts json string to dictionary
         return_val[_[0]] = eval(item)[_[0]]
     return return_val
 
+
 def fetchfromfile(filename: str):  # converts file with json string to dictionary
     with open(filename) as file:
         return_value = fetchjson(file)
     return return_value
 
+
 def fetchfromdata(number: str):
-    with open(f"C:\\Users\\Thomas\\Documents\\XandOdata{number}.json") as file:
+    with open(datafolderprefix.replace('*', number)) as file:
         return_value = fetchjson(file)
     return return_value
 
-def find_children(parentname: str):
+
+def findchildren(parentname: str):
     children = []
     for potential_child in range(1, 10):
         if str(potential_child) not in parentname:
             children.append(parentname + str(potential_child))
     return children
 
+
 def getdata(node: str):
     return fetchfromfile(datafolderprefix.replace('*', str(len(node))))[node]
 
+
 def getchilddata(parent: str):
-    child_file = fetchfromfile(datafolderprefix.replace('*', str(len(parent)+1)))
+    child_file = fetchfromfile(datafolderprefix.replace('*', str(len(parent) + 1)))
     return_dict = dict()
     for child in child_file:
         if child.startswith(parent):
             return_dict[child] = child_file[child]
     return return_dict
 
-def keys_with_top_values(my_dict: dict):
-    return [key for (key, value) in my_dict.items() if value == max(my_dict.values())]
 
-def choose_child(parent: str):
+def keyswithtopvalues(mydict: dict):
+    return [key for (key, value) in mydict.items() if value == max(mydict.values())]
+
+
+def choosechild(parent: str):
     roundnum = len(parent) + 1
     boarddata = fetchfromfile(datafolderprefix.replace('*', str(roundnum)))
-    win_ratio = 0
+    winratio = 0
 
-    choice_list = dict()
+    choicelist = dict()
 
-    for child in find_children(parent):
+    for child in findchildren(parent):
         data = getdata(child)
         wins = data['w']
         plays = data['p']
         templog = 0
-        parent_visits = 0
+        parentvisits = 0
         if parent == '':  # if the parent is blank, use the number of simulations
-            parent_visits = json.load(open("C:\\Users\\Thomas\\Documents\\extra.json"))['simulations']
+            parentvisits = json.load(open(os.path.dirname(os.path.realpath(__file__)) + "\\extra.json"))['simulations']
         else:
-            parent_visits = getdata(parent)['p']
+            parentvisits = getdata(parent)['p']
         if plays == 0:  # sets the factor to 10 to guarentee every one is used at least once
-            choice_factor = 10
+            choicefactor = 10
         else:
-            win_ratio = wins / plays
-            templog = log(parent_visits, e)
-            choice_factor = win_ratio + c * sqrt(templog/plays)
-        choice_list[child] = choice_factor
+            winratio = wins / plays
+            templog = log(parentvisits, e)
+            choicefactor = winratio + c * sqrt(templog / plays)
+        choicelist[child] = choicefactor
 
-    best_child = r.choice(keys_with_top_values(choice_list))
+    best_child = r.choice(keyswithtopvalues(choicelist))
 
     return best_child
 
-def have_turn(board_state: str, bot: bool):
+
+def haveturn(board_state: str, bot: bool):
     returnvalue = ''
     if bot:
-        returnvalue = choose_child(board_state)
+        returnvalue = choosechild(board_state)
     else:
         turn = input('>>> ')
-        if verify_turn(turn, board_state):
+        if verifyturn(turn, board_state):
             returnvalue = board_state + turn
         else:
-            have_turn(board_state, bot)
+            haveturn(board_state, bot)
 
     return returnvalue
 
-def play_simulation():
+
+def playsimulation():
     global first, prettyboard
-    first = r.choice([True, True])  # True if the bot is first, false otherwise
+    first = r.choice([False, True])  # True for the bot, false for humans
     playerpieces = {''}  # work out if which player is x or o
     if first:
         print('You are O')
@@ -115,18 +127,17 @@ def play_simulation():
         print('You are X')
     game = ''
     won = ''
-    player1 = ''.join(sorted(list(game[0::2])))
-    player2 = ''.join(sorted(list(game[1::2])))
+    print(prettyboard)
     while won == '' and len(game) < 9:
-        game = have_turn(game, first)
-        #first = not first
+        game = haveturn(game, first)
+        first = not first  # can be removed for zero or two player game
 
         player1 = ''.join(sorted(list(game[0::2])))
         player2 = ''.join(sorted(list(game[1::2])))
-        for item in winConditions:
-            if fully_contained(item, player1):
+        for item in winconditions:
+            if fullycontained(item, player1):
                 won = 1
-            elif fully_contained(item, player2):
+            elif fullycontained(item, player2):
                 won = 2
         for spot in player1:
             prettyboard = prettyboard.replace(spot, 'X')
@@ -139,12 +150,15 @@ def play_simulation():
     else:
         print('Tie')
 
-    update(game)
+    # update(game)
 
-def verify_turn(turn: str, state: str):
-    if turn in state:
-        return False
-    return True
+
+def verifyturn(turn: str, state: str):
+    if turn.isdigit() and len(str(turn)) == 1:
+        if turn not in state:
+            return True
+    return False
+
 
 def update(endgame: str):
     botplays = []
@@ -160,5 +174,5 @@ def update(endgame: str):
 """
 
 
-play_simulation()
+playsimulation()
 quit()
